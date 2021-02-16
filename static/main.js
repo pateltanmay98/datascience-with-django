92d3d6766f3e9abe162cec8f2b815eb829526bff
patel_tanmay98 <patelth1998@gmail.com>
2021-02-16 13:54:29 +0530

    Add files via upload

--- static/main.js
+++ static/main.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
 $('#select')
   .dropdown()
 ;
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
+$('#modal-btn').click(function(){
+   $('.ui.modal')
+   .modal('show');
+});
 })
