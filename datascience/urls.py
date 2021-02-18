cf5f56b0f35cd609a8f7163299d095fd969cc64c
patel_tanmay98 <patelth1998@gmail.com>
2021-02-18 16:35:06 +0530

    Add files via upload

--- datascience/urls.py
+++ datascience/urls.py
@@ -18,9 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from datascience import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls', namespace='products')),
+    path('', views.home_view, name='home'),
+    path('performance/', include('products.urls', namespace='products')),
     path('upload/', include('csvs.urls', namespace='csvs')),
     path('customers/', include('customers.urls', namespace='customers')),
 ]
